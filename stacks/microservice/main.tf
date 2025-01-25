e89705891cb3c457c6cd7b1d24324c6a8ea3401d
jaimeGarita <jaime.garita.camargo@gmail.com>
2025-01-25 18:20:54 +0100

    add ci-cd deployment pending check connection pending check build and deploy

--- stacks/microservice/main.tf
+++ stacks/microservice/main.tf
@@ -1,26 +1,141 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "microservice_s3" {
-  source      = "../modules/s3_bucket"
-  bucket_name = var.bucket_name
-  acl         = "private"
-}
-
-module "microservice_role" {
-  source        = "../modules/iam_role"
-  role_name     = var.role_name
+resource "aws_iam_role" "new_codepipeline_role" {
+  name = "GabiotaRM"  # Nombre más corto
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action    = "sts:AssumeRole"
-        Effect    = "Allow"
         Principal = {
-          Service = "ecs.amazonaws.com"
+          Service = "codepipeline.amazonaws.com"  # Solo CodePipeline, no codestar-connections
         }
+        Effect    = "Allow"
+        Sid       = ""
       }
     ]
   })
+}
+
+resource "aws_iam_policy" "codepipeline_permissions" {
+  name        = "CodePipelinePermissionsForBuild"
+  description = "Allow CodePipeline to interact with CodeBuild, CodeStarConnections, and S3"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "codepipeline:PollForJobs",
+          "codepipeline:PutJobFailureResult",
+          "codepipeline:PutJobSuccessResult"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = [
+          "codebuild:BatchGetBuilds",
+          "codebuild:StartBuild",
+          "codebuild:ListBuildsForProject"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::codepipelinestartertempla-codepipelineartifactsbuc-ceeedyolaysk/*"
+      },
+      {
+        Action   = [
+          "codestar-connections:UseConnection",
+          "codestar-connections:ListConnections",
+          "codestar-connections:GetConnection"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:codestar-connections:us-west-2:195275638124:connection/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_codepipeline_permissions" {
+  policy_arn = aws_iam_policy.codepipeline_permissions.arn
+  role       = aws_iam_role.new_codepipeline_role.name
+}
+
+resource "aws_codepipeline" "my_pipeline" {
+  name           = "SimpleDockerService"
+  execution_mode = "QUEUED"
+  pipeline_type  = "V2"
+  role_arn       = aws_iam_role.new_codepipeline_role.arn
+  tags           = {}
+  tags_all       = {}
+
+  artifact_store {
+    location = "codepipelinestartertempla-codepipelineartifactsbuc-ceeedyolaysk"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      category           = "Source"
+      configuration      = {
+        "BranchName"       = "main"
+        "ConnectionArn"    = aws_codestarconnections_connection.github_connection.arn
+        "FullRepositoryId" = "jaimeGarita/api-tfm"
+      }
+      input_artifacts    = []
+      name               = "CodeConnections"
+      output_artifacts   = ["SourceOutput"]
+      owner              = "AWS"
+      provider           = "CodeStarSourceConnection"
+      role_arn           = aws_iam_role.new_codepipeline_role.arn
+      run_order          = 1
+      version            = "1"
+    }
+  }
+
+  stage {
+    name = "Build_and_Deploy"
+
+    action {
+      category           = "Build"
+      configuration      = {
+        "ProjectName" = "SimpleDockerService"
+      }
+      input_artifacts    = ["SourceOutput"]
+      name               = "Docker_Build_Tag_and_Push"
+      output_artifacts   = []
+      owner              = "AWS"
+      provider           = "CodeBuild"
+      role_arn           = aws_iam_role.new_codepipeline_role.arn
+      run_order          = 1
+      version            = "1"
+    }
+  }
+
+  trigger {
+    provider_type = "CodeStarSourceConnection"
+
+    git_configuration {
+      source_action_name = "CodeConnections"
+
+      push {
+        branches {
+          includes = [
+            "main",
+          ]
+        }
+      }
+    }
+  }
+}
+
+resource "aws_codestarconnections_connection" "github_connection" {
+  name         = "api-tfm-connection"
+  provider_type = "GitHub"
 }
--- stacks/microservice/outputs.tf
+++ stacks/microservice/outputs.tf
@@ -1,9 +1,16 @@
-output "bucket_name" {
-  description = "El nombre del bucket S3 creado para el datalake"
-  value       = module.microservice_s3.bucket_name
-}
+# # outputs.tf
 
-output "role_name" {
-  description = "El nombre del rol IAM creado para el microservicio"
-  value       = module.microservice_role.role_name
-}
+# output "pipeline_name" {
+#   description = "Nombre del pipeline de CodePipeline"
+#   value       = aws_codepipeline.pipeline.name
+# }
+
+# output "ecr_repository_url" {
+#   description = "URL del repositorio de ECR"
+#   value       = aws_ecr_repository.app_repo.repository_url
+# }
+
+# output "aws_s3_bucket" {
+#   description = "Nombre del bucket S3 utilizado por el pipeline"
+#   value       = aws_s3_bucket.pipeline_bucket.bucket
+# }
