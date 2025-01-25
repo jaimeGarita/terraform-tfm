1f91f6fd8e0e9cda818bcb4c8dc17b8de614e80a
Lautaro Olivares Sierra <lautaro.olivaressierra@pomelo.la>
2025-01-25 15:36:58 +0100

    first commit

--- modules/iam_role/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_role" "role" {
-  name               = var.role_name
-  assume_role_policy = var.assume_role_policy
-}
-
-output "role_name" {
-  value = aws_iam_role.role.name
-}
---
+++ modules/rds_to_datalake/variables.tf
@@ -0,0 +1,87 @@
+variable "region" {
+  type        = string
+  description = "Full name of the region"
+}
+
+variable "environment" {
+  type    = string
+  default = "infra"
+}
+
+variable "region_alias" {
+  description = "Short name for the AWS region, to be used as a component of resource names."
+  type        = string
+}
+
+variable "service" {
+  description = "Name of the service"
+  type        = string
+}
+
+variable "business_unit" {
+  description = "Name of the business unit. It will be used as a component of resource names."
+  type        = string
+}
+
+variable "account_id" {
+  description = "The AWS account ID"
+  type        = string
+}
+
+variable "cluster" {
+  description = "The name of the RDS cluster"
+  type        = string
+}
+
+variable "database_name" {
+  description = "The name of the database"
+  type        = string
+}
+
+variable "secrets_manager_arn" {
+  description = "The ARN of the secret to access the RDS database"
+  type        = string
+}
+
+variable "replication_instance_arn" {
+  description = "The ARN of the DMS replication instance"
+  type        = string
+}
+
+variable "purpose" {
+  type    = string
+  default = "datalake"
+}
+
+variable "compliance" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the RDS instance will be created"
+  type        = string
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Application  = var.service
+      Team         = "data"
+      BusinessUnit = "data-fraud"
+      Compliance   = var.compliance
+      Environment  = var.environment
+      Monitoring   = "true"
+      Provisioned  = "terraform"
+      Region       = var.region
+      Geography    = "global"
+      Repository   = "static-infra"
+      Tenant       = "shared"
+    }
+  }
+}
